9ae9cc22bfec1eaa8bd3fa278a310f8b682e64b4
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-05-21 13:10:51 +0200

    Update api.js

--- myprofilee/src/data/api.js
+++ myprofilee/src/data/api.js
@@ -1,11 +1,12 @@
-const apis =[
-
+// Define an array of API data
+const apis = [
     {
-
-        link:"https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg ",
-        fullName : " Cristiano Ronaldo",
-        bio: "  Cristiano Ronaldo a pour surnom CR7. Il est né à Funchal sur l'île de Madère le 5 février 1985",
-        profession:"un footballeur  professional",
+        link: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg",
+        fullName: "Cristiano Ronaldo",
+        bio: "Cristiano Ronaldo, nicknamed CR7, was born in Funchal on the island of Madeira on February 5, 1985.",
+        profession: "A professional football player",
     },
-]
-export default apis
+];
+
+// Export the apis array as the default export
+export default apis;
