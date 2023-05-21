59f10f5b243f7830c957b97cb811fffbdd6734d9
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-05-21 13:08:14 +0200

    Update Carddd.js

--- myprofilee/src/component/profile/Carddd.js
+++ myprofilee/src/component/profile/Carddd.js
@@ -1,29 +1,40 @@
-import  React from 'react'
+// Import necessary dependencies from react and react-bootstrap
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+
+// Import API data from a specific location
 import apis from '../../data/api';
+
+// Define a functional component called Carddd
 const Carddd = (prpos) => {
-    const link=prpos.link;
-    const  fullName=prpos. fullName;
-    const bio=prpos.bio;
-    const  profession= prpos. profession;
-  return (
-    <div>
+    // Destructure the props object to get specific values
+    const link = prpos.link;
+    const fullName = prpos.fullName;
+    const bio = prpos.bio;
+    const profession = prpos.profession;
 
-<Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={link} />
-      <Card.Body>
-        <Card.Title>{fullName} </Card.Title>
-        <Card.Text>
-         {bio} {profession}
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    
-
-    </div>
-  )
+    // Render the component
+    return (
+        <div>
+            {/* Render a Bootstrap Card component */}
+            <Card style={{ width: '18rem' }}>
+                {/* Render an image within the Card */}
+                <Card.Img variant="top" src={link} />
+                <Card.Body>
+                    {/* Render the full name within the Card Title */}
+                    <Card.Title>{fullName}</Card.Title>
+                    <Card.Text>
+                        {/* Render the bio and profession within the Card Text */}
+                        {bio} {profession}
+                    </Card.Text>
+                    {/* Render a Bootstrap Button within the Card Body */}
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 }
 
-export default Carddd
+// Export the Carddd component as the default export
+export default Carddd;
