4edc51f2913a67dbe3b81ed80fc33b7342cf4ff9
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-05-21 13:09:34 +0200

    Update Containersyst.js

--- myprofilee/src/component/profile/Containersyst.js
+++ myprofilee/src/component/profile/Containersyst.js
@@ -1,34 +1,39 @@
-import  React from 'react'
-import apis from '../../data/api'
-import Carddd from './Carddd'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+// Import necessary dependencies from React and react-bootstrap
+import React from 'react';
+import apis from '../../data/api';
+import Carddd from './Carddd';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
+// Define a functional component called Containersyst
 const Containersyst = () => {
- 
+
+  // Render the component
   return (
     <div>
-<Container>
-    <Row>
-        {apis.map(({link, fullName ,bio ,profession})=>
-        <Col>
-        <Carddd
-        link={ link}
-        fullName={fullName}
-        bio={bio}
-        profession={profession}
-        
-        />
-       </Col>
-      )}
-    </Row>
-
-
-</Container>
-
+      {/* Render a Bootstrap Container */}
+      <Container>
+        {/* Render a Bootstrap Row */}
+        <Row>
+          {/* Iterate over the array of API data */}
+          {apis.map(({ link, fullName, bio, profession }) => (
+            // Render a Bootstrap Col for each item in the array
+            <Col>
+              {/* Render the Carddd component and pass props */}
+              <Carddd
+                link={link}
+                fullName={fullName}
+                bio={bio}
+                profession={profession}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Containersyst
+// Export the Containersyst component as the default export
+export default Containersyst;
