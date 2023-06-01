3aabad13c7459a34638a20c43fef019cc1aad4ea
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-06-01 20:53:32 +0200

    Update App.js

--- myprofilee/src/App.js
+++ myprofilee/src/App.js
@@ -1,12 +1,19 @@
 
-import './App.css';
+{/* importation de la styleSheet du boostrap */}
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+{/* importation de nos composants */}
+
 import Containersyst from './component/profile/Containersyst';
+{/* fonction App */}
+
 function App() {
   
   
   return (
    <>
+    {/* importation de notre composants Containersyst */}
+
    <Containersyst
    />
    
